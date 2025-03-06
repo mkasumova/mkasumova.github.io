f3e115100e4a817ccef264326f33bd3612184396
mkasumova <mariyakas07@gmail.com>
2025-03-05 20:58:13 -0500

    Add files via upload

---
+++ julia/hotdog/javascript/hotdog.js
@@ -0,0 +1,13 @@
+function toggleItem(mustache) {
+   
+    let item = document.getElementById(mustache);
+   
+    item.style.display = (item.style.display === "none" || item.style.display === "") ? "block" : "none";
+}
+
+function toggleItem(wig) {
+   
+    let item = document.getElementById(wig);
+   
+    item.style.display = (item.style.display === "none" || item.style.display === "") ? "block" : "none";
+}
